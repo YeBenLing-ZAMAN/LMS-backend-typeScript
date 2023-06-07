0c4d4550660044f8f22158ce91d96e03169dfa60
md kamruzzaman <kamruzzamanmd@hotmail.com>
2023-06-07 12:43:35 +0600

    setup eslint, husky, lin-staged done

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,9 +2,7 @@ import dotenv from "dotenv";
 import path from "path";
 dotenv.config({ path: path.join(process.cwd(), ".env") }); // current working directory meant
 
-
-
 export default {
-    port : process.env.PORT,
-    dataBase_url : process.env.DB_URL
-}
+  port: process.env.PORT,
+  dataBase_url: process.env.DB_URL,
+};
