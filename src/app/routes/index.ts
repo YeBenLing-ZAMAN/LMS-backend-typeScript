23f570e91306ded1863a7b4661ceb3a8f3620e90
md kamruzzaman <kamruzzamanmd@hotmail.com>
2023-06-12 16:14:32 +0600

    department rounte added [done]

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { UserRoutes } from "../modules/users/user.routes";
 import { AcademicSemesterRoutes } from "../modules/academicSemester/academicSemester.router";
 import { AcademicFacultyRoutes } from "../modules/academicFaculty/academicFaculty.route";
+import { academicDepartmentRoutes } from "../modules/academicDepartment/academicDepartment.route";
 
 const router = express.Router();
 
@@ -9,6 +10,7 @@ const moduleRoutes = [
   { path: "/user", route: UserRoutes },
   { path: "/academic-semester", route: AcademicSemesterRoutes },
   { path: "/academic-faculties", route: AcademicFacultyRoutes },
+  { path: "/academic-departments", route: academicDepartmentRoutes },
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
