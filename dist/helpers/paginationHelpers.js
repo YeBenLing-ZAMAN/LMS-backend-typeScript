aac2ae4cf7240d4e9524fe5cc8ffed3be8c8c166
md kamruzzaman <kamruzzamanmd@hotmail.com>
2023-06-14 09:27:39 +0600

    [done] deployment env setup

--- dist/helpers/paginationHelpers.js
+++ dist/helpers/paginationHelpers.js
@@ -2,19 +2,19 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.paginationHelper = void 0;
 const calculatePagination = (options) => {
-    const page = Number(options.page || 1);
-    const limit = Number(options.limit || 10);
-    const skip = (page - 1) * limit;
-    const sortBy = options.sortBy || "createdAt";
-    const sortOrder = options.sortOrder || "desc";
-    return {
-        page,
-        limit,
-        skip,
-        sortBy,
-        sortOrder,
-    };
+  const page = Number(options.page || 1);
+  const limit = Number(options.limit || 10);
+  const skip = (page - 1) * limit;
+  const sortBy = options.sortBy || "createdAt";
+  const sortOrder = options.sortOrder || "desc";
+  return {
+    page,
+    limit,
+    skip,
+    sortBy,
+    sortOrder,
+  };
 };
 exports.paginationHelper = {
-    calculatePagination,
+  calculatePagination,
 };
