29c6308418fa44416958f835bf19933efd91c65c
md kamruzzaman <kamruzzamanmd@hotmail.com>
2023-10-21 13:51:57 +0600

    [issue-solved]: global error handle by prisma

--- core-service/src/app/modules/semesterRegistration/semesterRegistration.service.ts
+++ core-service/src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -1,36 +1,43 @@
-import { SemesterRegistration, SemesterRegistrationStatus } from "@prisma/client";
-import httpStatus from "http-status";
-import ApiError from "../../../errors/ApiError";
-import prisma from "../../../shared/prisma";
-
-const insertIntoDB = async (data: SemesterRegistration): Promise<SemesterRegistration> => {
-
-    const isAnySemesterRegUpcomingOrOngoing = await prisma.semesterRegistration.findFirst({
+import {
+    SemesterRegistration,
+    SemesterRegistrationStatus,
+  } from '@prisma/client';
+  import httpStatus from 'http-status';
+  import ApiError from '../../../errors/ApiError';
+  import prisma from '../../../shared/prisma';
+  
+  const insertIntoDB = async (
+    data: SemesterRegistration
+  ): Promise<SemesterRegistration> => {
+    const isAnySemesterRegUpcomingOrOngoing =
+      await prisma.semesterRegistration.findFirst({
         where: {
-            OR: [
-                {
-                    status: SemesterRegistrationStatus.UPCOMING
-                },
-                {
-                    status: SemesterRegistrationStatus.ONGOING
-                }
-            ]
-        }
-    })
-
+          OR: [
+            {
+              status: SemesterRegistrationStatus.UPCOMING,
+            },
+            {
+              status: SemesterRegistrationStatus.ONGOING,
+            },
+          ],
+        },
+      });
+  
     if (isAnySemesterRegUpcomingOrOngoing) {
-        throw new ApiError(httpStatus.BAD_REQUEST,
-            `Thers is already an ${isAnySemesterRegUpcomingOrOngoing.status} registration.`
-        )
+      throw new ApiError(
+        httpStatus.BAD_REQUEST,
+        `Thers is already an ${isAnySemesterRegUpcomingOrOngoing.status} registration.`
+      );
     }
-
+  
     const result = await prisma.semesterRegistration.create({
-        data
-    })
-
+      data,
+    });
+  
     return result;
-}
-
-export const SemesterRegistrationService = {
-    insertIntoDB
-}
+  };
+  
+  export const SemesterRegistrationService = {
+    insertIntoDB,
+  };
+  
